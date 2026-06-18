8f9914209ad10fb4d540dc5a8d4da9b0d89b46ff
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-06-18 23:39:26 +0530

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,7 @@
 let rows = 5
 
 for (let index = 0; index < rows; index++) {
-    const space = "-".repeat(rows - index - 1)
+    const space = " ".repeat(rows - index - 1)
     const star = "*".repeat(2 * index + 1)
     console.log(space + star)
 }
